901782afb15494002df5f786aed75fd3d2542f32
Estelope <elopez14567@gmail.com>
2023-06-21 00:13:34 -0600

    fix syntax issues regarding inputs in js

--- Js/scripts.js
+++ Js/scripts.js
@@ -3,28 +3,30 @@ function handleRadio(event) {
   event.preventDefault();
   const radioSelection = document.querySelector("input[name='flavor']:checked").value;
   // let checkedValue =document.getElementById("choc").value;
-  
-}
+  if (typeof radioSelection === "1") {
+    console.log('Variable is undefined');
+  }
+  // used this for troubleshooting and coudlnt get my input values to show for dropdown forms or radioSelection variable. 
+  else if(radioSelection === "1") {
+    document.getElementById("answer1").removeAttribute("class","hidden")
+    console.log('variable is submitting');
+  }
+  else if (radioSelection === "2") {
+    document.getElementById("answer2").removeAttribute("class","hidden");
+  }
+  else if (radioSelection === "3") {
+    document.getElementById("answer3").removeAttribute("class","hidden");
+  }
+    
+  };
+
 
 
 window.addEventListener("load", function() {
   document.getElementById("radio-form").addEventListener("submit", handleRadio);
 
   
-if (typeof radioSelection === 'undefined') {
-  console.log('Variable is undefined');
-}
-// used this for troubleshooting and coudlnt get my input values to show for dropdown forms or radioSelection variable. 
-else if(radioSelection === "choc") {
-  document.getElementById("answer1").removeAttribute("class","hidden")
-  console.log('variable is submitting');
-}
-else if (radioSelection === "van") {
-  document.getElementById("answer2").removeAttribute("class","hidden");
-}
-else if (radioSelection === "cookies") {
-  document.getElementById("answer3").removeAttribute("class","hidden");
-}
+
   
 });
 
@@ -33,21 +35,7 @@ else if (radioSelection === "cookies") {
 
 
 
-let result;
-if (typeof radioSelection === 'undefined') {
-  console.log('Variable is undefined');
-}
-// used this for troubleshooting and coudlnt get my input values to show for dropdown forms or radioSelection variable. ive spent over 7 hours troubleshooting any hints would help.
-else if(radioSelection === "1") {
-  document.getElementById("answer1").removeAttribute("class","hidden")
-  console.log('variable is submitting');
-}
-else if (radioSelection === "2") {
-  document.getElementById("answer2").removeAttribute("class","hidden");
-}
-else if (radioSelection === "3") {
-  document.getElementById("answer3").removeAttribute("class","hidden");
-}
+
 
 
 
