b4594bbb1cff1cbaf67fba7b4bff00d8ed03913f
Estelope <elopez14567@gmail.com>
2023-06-21 01:10:45 -0600

    fix indenting, add additional CSS styling

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,11 +1,11 @@
 function handleRadio(event) {
   event.preventDefault();
   const radioSelection = document.querySelector("input[name='flavor']:checked").value;
-  
+
   if (typeof radioSelection === "undefined") {
     console.log('Variable is undefined');
   }
-  
+
   else if (radioSelection === "1") {
     document.getElementById("answer1").removeAttribute("class", "hidden");
     document.getElementById("answer2").setAttribute("class", "hidden");
@@ -24,14 +24,8 @@ function handleRadio(event) {
 
 };
 
-
-
 window.addEventListener("load", function () {
   document.getElementById("radio-form").addEventListener("submit", handleRadio);
-
-
-
-
 });
 
 
